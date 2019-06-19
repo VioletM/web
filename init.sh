51c4a336f35522499976a411a9ee342e57ac006c
mishechk <violetta.mishechkina.ext@nokia.com>
2019-06-19 17:38:20 +0300

    Revert "first_task"
    This reverts commit 270b72ea

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
 sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
