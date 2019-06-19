2b9edb942c8d2b2903c21b7db9ef960d7792328e
mishechk <violetta.mishechkina.ext@nokia.com>
2019-06-19 17:52:03 +0300

    second task

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
+sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/conf.d/nginx.conf
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
 sudo rm /etc/nginx/sites-enabled/default
