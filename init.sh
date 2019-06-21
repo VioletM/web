d4878758208c4f5d82bf3bf88f309c542a356093
mishechk <violetta.mishechkina.ext@nokia.com>
2019-06-21 14:47:04 +0300

    third task

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
 sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo gunicorn -c config.conf
