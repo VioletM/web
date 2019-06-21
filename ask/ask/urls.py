2bd868ee7e4c97e1524ce9c7cc1590c65c96c5c8
mishechk <violetta.mishechkina.ext@nokia.com>
2019-06-21 16:16:02 +0300

    third task

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.urls import include
+from django.conf.urls import include
 
 urlpatterns = [
-    url(r'^$', include('qa.urls'))
+    url(r'^$', include('qa.urls')),
+    url(r'^login/', include('qa.urls')),
+    url(r'^signup/', include('qa.urls')),
+    url(r'^question/\d\d\d/', include('qa.urls')),
+    url(r'^ask/', include('qa.urls')),
+    url(r'^popular/', include('qa.urls')),
+    url(r'^new/', include('qa.urls')),
 ]
