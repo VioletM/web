2e2ae1dd312e0df5959adfc2d35d53946946d41f
mishechk <violetta.mishechkina.ext@nokia.com>
2019-07-09 13:19:05 +0300

    third task

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
 
-# Create your models here.
+class Question(models.Model):
+
+    class QuestionManager(models.Manager):
+        def new(self):
+            return self.order_by('-added_at')
+
+        def popular(self):
+            return self.order_by('-rating')
+
+    object = QuestionManager()
+    title = models.CharField(250)
+    text = models.TextField()
+    added_at = models.DateField(default=dt.now)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(default=dt.now)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
+
+
+
--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
+# MySQL
+sudo /etc/init.d/mysql start
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+# nginx and gunicorn
 sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
 sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
 sudo rm /etc/nginx/sites-enabled/default
