f287e0098b3bc7101fd17d5b6fcb248b595855c8
mishechk <violetta.mishechkina.ext@nokia.com>
2019-07-10 18:14:52 +0300

    sixth task

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,9 +4,9 @@ from qa.views import test, main_page, popular_page, question_page, ask_page
 
 urlpatterns = [
     url(r'^question/(?P<num>\d+)', question_page, name='question-page'),
-    url(r'^popular/', popular_page, name='popular-page'),
+    url(r'^popular', popular_page, name='popular-page'),
     url(r'^$', main_page, name='main-page'),
     url(r'^login/', test),
     url(r'^signup/', test),
-    url(r'^ask/', ask_page, name='ask-page'),
+    url(r'^ask', ask_page, name='ask-page'),
 ]
