4918fdbc97c8e67182b05ef9dd337e5be9886b4b
mishechk <violetta.mishechkina.ext@nokia.com>
2019-07-10 12:32:46 +0300

    fifth task

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -10,11 +10,13 @@ logger = logging.getLogger(__name__)
 
 @require_GET
 def test(request, *args, **kwargs):
+    logger = logging.getLogger(__name__)
     logger.debug('test')
     return HttpResponse('OK')
 
 @require_GET
 def main_page(request, *args, **kwargs):
+    logger = logging.getLogger(__name__)
     logger.debug('main_page')
     page = int(request.GET.get('page', 1))
     limit = 10
@@ -35,6 +37,7 @@ def main_page(request, *args, **kwargs):
 
 @require_GET
 def popular_page(request, *args, **kwargs):
+    logger = logging.getLogger(__name__)
     logger.debug('popular_page')
     page = int(request.GET.get('page'))
     limit = 10
@@ -51,6 +54,7 @@ def popular_page(request, *args, **kwargs):
 
 @require_GET
 def question_page(request, **kwargs):
+    logger = logging.getLogger(__name__)
     logger.debug('question_page')
     num = int(kwargs.get('num'))
     question = get_object_or_404(Question, id=num)
