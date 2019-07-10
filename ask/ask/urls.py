27cdf186ce088ce994ad9e2e62cb39c04c8e2889
mishechk <violetta.mishechkina.ext@nokia.com>
2019-07-10 12:51:26 +0300

    fifth task

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,10 +17,10 @@ from django.conf.urls import url
 from django.conf.urls import include
 
 urlpatterns = [
-    url(r'^', include('qa.urls')),
+    url(r'^question/\d+', include('qa.urls')),
+    url(r'^$', include('qa.urls')),
     url(r'^login/', include('qa.urls')),
     url(r'^signup/', include('qa.urls')),
-    url(r'^question/\d+', include('qa.urls')),
     url(r'^ask/', include('qa.urls')),
     url(r'^popular/', include('qa.urls')),
 ]
